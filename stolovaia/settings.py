717696e5848cdea0fda6e1d6b55ccc72336b54a3
pyataninastesha <pyataninastefa@yandex.ru>
2026-02-10 22:30:10 +0300

    1234567

--- stolovaia/settings.py
+++ stolovaia/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = False
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
