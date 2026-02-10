1f7401241e274e08b051331972b0674a1d277986
pyataninastesha <pyataninastefa@yandex.ru>
2026-02-10 19:25:26 +0300

    1234567

--- users/urls.py
+++ users/urls.py
@@ -9,12 +9,6 @@ urlpatterns = [
     path('profile/', views.profile_view, name='profile'),
     path('wallet/', views.wallet_view, name='wallet'),
     path('banquets/', views.banquet_purchase_view, name='banquets'),
-    path('subscription/', views.subscription_view, name='subscription'),
-    path('subscription/cancel/<int:sub_id>/', views.subscription_cancel, name='subscription_cancel'),
-    path('receive-meal/', views.receive_meal_view, name='receive_meal'),
-    path('receive-meal/request/', views.request_meal, name='request_meal'),
-    path('receive-meal/cancel/<int:request_id>/', views.cancel_meal_request, name='cancel_meal_request'),
-    path('receive-meal/confirm/<int:request_id>/', views.confirm_meal, name='confirm_meal'),
 
 
 ]
